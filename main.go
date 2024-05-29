a71e75370593a0902a8d41ed63d45fce17421744
Artem Vinogradov <vinartgen@outlook.com>
2024-05-29 15:18:53 +0300

    create html templates

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
+
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
+
+func loarTemplates() {
+	templateName := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateName {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		}else {
+			panic(err)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("TODO: add some features")
+	loarTemplates()
 }
